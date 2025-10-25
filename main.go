19d1e5ede9a4c86dfa509530f423cc59b1be1451
Kien <ntkien92@gmail.com>
2025-10-25 14:23:11 +0700

    refactor code

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -16,8 +17,8 @@ import (
 
 type GoldPrice struct {
 	Type      string    `json:"type"`
-	Buy       string    `json:"buy"`
-	Sell      string    `json:"sell"`
+	Buy       int64     `json:"buy"`
+	Sell      int64     `json:"sell"`
 	Converted string    `json:"converted"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
@@ -27,6 +28,7 @@ type Config struct {
 	TelegramChatID string `json:"telegram_chat_id"`
 	SlackWebhook   string `json:"slack_webhook"`
 	FormatTime     string `json:"format_time"`
+	GptKey         string `json:"gpt_key"`
 }
 
 func loadConfig() Config {
@@ -63,10 +65,20 @@ func fetchGoldPrices() ([]GoldPrice, error) {
 		}
 		cells := s.Find("td")
 		if cells.Length() >= 3 {
+			buy, err := convertToInt64(strings.TrimSpace(cells.Eq(1).Text()))
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				return
+			}
+			sell, err := convertToInt64(strings.TrimSpace(cells.Eq(2).Text()))
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				return
+			}
 			prices = append(prices, GoldPrice{
 				Type:      strings.TrimSpace(cells.Eq(0).Text()),
-				Buy:       strings.TrimSpace(cells.Eq(1).Text()),
-				Sell:      strings.TrimSpace(cells.Eq(2).Text()),
+				Buy:       *buy,
+				Sell:      *sell,
 				Converted: strings.TrimSpace(cells.Eq(3).Text()),
 				UpdatedAt: time.Now(),
 			})
@@ -128,13 +140,93 @@ func sendSlack(cfg Config, message string) {
 	http.Post(cfg.SlackWebhook, "application/json", strings.NewReader(payload))
 }
 
-func formatMessage(cfg Config, prices []GoldPrice) string {
+func formatMessage(cfg Config, prices []GoldPrice, lastPrices []GoldPrice) (*string, error) {
 	message := fmt.Sprintf("Giá vàng hôm nay - %s 💰\n", time.Now().Format(cfg.FormatTime))
+	var ringGold GoldPrice
+	var lastRingGold GoldPrice
+
 	for _, p := range prices {
-		message += fmt.Sprintf("• %s: Mua %s - Bán %s\n", p.Type, p.Buy, p.Sell)
+		if p.Type == "Vàng nhẫn khâu 9999" {
+			ringGold = p
+		}
+	}
+	for _, p := range lastPrices {
+		if p.Type == "Vàng nhẫn khâu 9999" {
+			lastRingGold = p
+		}
 	}
 
-	return message
+	message += fmt.Sprintf("• %s: Mua %s/chỉ - Bán %s/chỉ\n", ringGold.Type, FormatVND(ringGold.Buy), FormatVND(ringGold.Sell))
+	currentBuy := ringGold.Buy
+	currentSell := ringGold.Sell
+	lastBuy := lastRingGold.Buy
+	lastSell := lastRingGold.Sell
+
+	switch true {
+	case currentBuy-lastBuy > 0:
+		message += fmt.Sprintf("> Hôm nay giá mua tăng %s/chỉ so với trước đó\n", FormatVND(currentBuy-lastBuy))
+	case currentBuy-lastBuy < 0:
+		message += fmt.Sprintf("> Hôm nay giá mua giảm %s/chỉ so với trước đó\n", FormatVND(currentBuy-lastBuy))
+	default:
+		message += fmt.Sprint("> Hôm nay giá mua không đổi so với trước đó\n")
+	}
+
+	switch true {
+	case currentSell-lastSell > 0:
+		message += fmt.Sprintf("> Hôm nay giá bán tăng %s/chỉ so với trước đó\n", FormatVND(currentSell-lastSell))
+	case currentSell-lastSell < 0:
+		message += fmt.Sprintf("> Hôm nay giá bán giảm %s/chỉ so với trước đó\n", FormatVND(currentSell-lastSell))
+	default:
+		message += fmt.Sprint("> Hôm nay giá bán không đổi so với trước đó\n")
+	}
+
+	return &message, nil
+}
+
+func convertToInt64(valueStr string) (*int64, error) {
+	valueStr = strings.ReplaceAll(valueStr, ",", "")
+
+	value, err := strconv.ParseInt(valueStr, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &value, nil
+}
+
+func loadLastPrices() []GoldPrice {
+	data, err := os.ReadFile("latest.json")
+	if err != nil {
+		log.Fatalf("Không đọc được latest.json: %v", err)
+	}
+	var prices []GoldPrice
+	json.Unmarshal(data, &prices)
+	return prices
+}
+
+func FormatVND(n int64) string {
+	n = n * 1000
+	if n < 0 {
+		n *= -1
+	}
+	s := fmt.Sprintf("%d", n)
+	var result strings.Builder
+
+	count := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		result.WriteByte(s[i])
+		count++
+		if count%3 == 0 && i != 0 {
+			result.WriteByte('.')
+		}
+	}
+
+	runes := []rune(result.String())
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes) + " ₫"
 }
 
 func main() {
@@ -144,8 +236,8 @@ func main() {
 		now := time.Now()
 		var nextRun time.Time
 
-		morning := time.Date(now.Year(), now.Month(), now.Day(), 9, 0, 0, 0, now.Location())
-		afternoon := time.Date(now.Year(), now.Month(), now.Day(), 17, 1, 0, 0, now.Location())
+		morning := time.Date(now.Year(), now.Month(), now.Day(), 3, 0, 0, 0, now.Location())
+		afternoon := time.Date(now.Year(), now.Month(), now.Day(), 8, 0, 0, 0, now.Location())
 
 		if now.Before(morning) {
 			nextRun = morning
@@ -165,12 +257,18 @@ func main() {
 			continue
 		}
 
+		lastPrices := loadLastPrices()
+
 		data, _ := json.MarshalIndent(prices, "", "  ")
 		os.WriteFile("latest.json", data, 0644)
 		saveToSQLite(prices)
-		message := formatMessage(cfg, prices)
-		sendTelegram(cfg, message)
-		sendSlack(cfg, message)
+		message, err := formatMessage(cfg, prices, lastPrices)
+		if err != nil {
+			log.Println("❌ Lỗi lấy message:", err)
+			continue
+		}
+		sendTelegram(cfg, *message)
+		sendSlack(cfg, *message)
 
 		log.Println("✅ Cập nhật giá vàng thành công:", time.Now())
 	}
